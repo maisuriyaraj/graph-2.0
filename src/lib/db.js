397937a2eebcf34f3d605317e9c9fb16ca81661e
maisuriyaraj <maisuriyaraj664@gmail.com>
2024-07-24 22:31:12 +0530

    Build

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 
 // const mongoURL = "mongodb+srv://maisuriyaraj664:zTZ4pj2LioNWbTPG@cluster0.dr24uoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-const mongoURL = process.env.DB_URL || "mongodb+srv://maisuriyaraj664:zTZ4pj2LioNWbTPG@cluster0.dr24uoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// mongodb+srv://maisuriyaraj664:zTZ4pj2LioNWbTPG@cluster0.dr24uoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+const mongoURL = process.env.DB_URL || "mongodb://localhost:27017/graphcommunity"
 await mongoose.connect(mongoURL).then(() => {
     console.log("Database Connected 🗿 !")
 })
