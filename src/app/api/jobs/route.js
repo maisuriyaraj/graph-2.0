349b94d3a8c3a85d53994143bc96e0469f42e6a2
maisuriyaraj <maisuriyaraj664@gmail.com>
2024-07-24 23:08:10 +0530

    Code Commit

--- src/app/api/jobs/route.js
+++ src/app/api/jobs/route.js
@@ -4,15 +4,8 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
     try {
-        const url = new URL(request.url);
-        const jobId = url.searchParams.get('jobId') || null;
-        if (jobId) {
-            const job = await jobListingModel.findOne({ _id: jobId });
-            return NextResponse.json({ status: true, message: "Job Data Fetched Successfully !", data: job });
-        } else {
             const jobLists = await jobListingModel.find({});
             return NextResponse.json({ status: true, message: "All Jobs Fetch Successfully !", data: jobLists });
-        }
     } catch (error) {
         console.error(error)
         return NextResponse.json({ status: false, message: "Unable to provide service !" }, { status: 500 });
