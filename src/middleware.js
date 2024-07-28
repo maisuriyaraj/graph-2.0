7ae24090e7835628c6d785850dc3328465b3e47d
maisuriyaraj <maisuriyaraj664@gmail.com>
2024-07-28 10:24:31 +0530

    commit-mid

--- src/middleware.js
+++ src/middleware.js
@@ -4,6 +4,8 @@ export function middleware(request) {
   // Get the cookie from the request headers
   const cookie = request.headers.get('cookie');
   let userCookie = null;
+  const res = NextResponse.next()
+
 
   if (cookie) {
     // Parse the cookie
@@ -24,11 +26,20 @@ export function middleware(request) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
+  // add the CORS headers to the response
+  res.headers.append('Access-Control-Allow-Credentials', "true")
+  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.headers.append(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
   // Continue to the next middleware or to the requested page
-  return NextResponse.next();
+  return res;
 }
 
 // Define the matcher to apply this middleware to specific routes
 export const config = {
-  matcher: ['/dashboard/:path*','/signin','/signup'],  // Apply middleware to /dashboard and its subroutes
+  matcher: ['/dashboard/:path*','/signin','/signup','/api/:path*'],  // Apply middleware to /dashboard and its subroutes
 };
