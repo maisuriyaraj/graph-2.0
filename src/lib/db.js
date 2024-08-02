3243dc16b19f196cd63d85f0d5d5530c90060fbd
maisuriyaraj <maisuriyaraj664@gmail.com>
2024-08-02 08:38:01 +0530

    code commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,13 +4,13 @@ import mongoose from "mongoose";
 // const mongoURL = "mongodb+srv://maisuriyaraj664:zTZ4pj2LioNWbTPG@cluster0.dr24uoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // mongodb+srv://maisuriyaraj664:zTZ4pj2LioNWbTPG@cluster0.dr24uoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 const mongoURL = process.env.DB_URL || "mongodb+srv://maisuriyaraj664:zTZ4pj2LioNWbTPG@cluster0.dr24uoe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-mongoose.connect(mongoURL,{
+mongoose.connect(mongoURL, {
     useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000, // Default is 30 seconds
-  socketTimeoutMS: 45000, // Default is 0 (no timeout)
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Default is 30 seconds
+    socketTimeoutMS: 45000, // Default is 0 (no timeout)
 }).then(() => {
     console.log("Database Connected 🗿 !")
-}).catch((error)=>{
+}).catch((error) => {
     console.log(error);
 })
